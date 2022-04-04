79e55e14e3fdc36d8e69727e1b24a27df11c6379
Naeem-wq <95334435+Naeem-wq@users.noreply.github.com>
2022-04-04 13:58:30 +0200

    .

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+function funct(){
+    document.getElementById("textBox").sort = "input"
+    document.getElementById("textBox").innerHTML = "so";
+}
+
+
+
+document.getElementById("count").style.color = "red"
