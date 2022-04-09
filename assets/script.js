1c0ae746a5357bc5ab45e2571ff1294e5b2fba5b
Naeem-wq <95334435+Naeem-wq@users.noreply.github.com>
2022-04-09 16:27:45 +0200

    Final Product
    Created a filter app that sorts words alphabetically. Also created a word count app.

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,33 @@
-function funct(){
-    document.getElementById("textBox").sort = "input"
-    document.getElementById("textBox").innerHTML = "so";
+// Word Soup js
+
+let myInput = document.getElementById ('inputBox');
+let myOutputBox = document.getElementById('result');
+
+let characterArray = [];
+
+function sortFunction(){
+    let inputValue = myInput.value;
+    console.log(inputValue);
+    characterArray = inputValue.split("");
+    console.log(characterArray);
+    characterArray.sort();
+    console.log(characterArray);
+    let theOutput = characterArray.join("");
+    console.log(theOutput);
+    myOutputBox.innerHTML = (theOutput);
 }
 
 
-
+// Character Count Js
 document.getElementById("count").style.color = "red"
+
+function countChars(obj){
+    var maxLength = 20;
+    var strLength = obj.value.length;
+    
+    if(strLength > maxLength){
+        document.getElementById("count").innerHTML = '<span>'+strLength+' out of '+maxLength+' characters</span>';
+    }else{
+        document.getElementById("count").innerHTML = strLength+' out of '+maxLength+' characters';
+    }
+}
